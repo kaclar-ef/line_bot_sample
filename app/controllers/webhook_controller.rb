56cf16a3b1ccfc2a5d398151b44bc79606aad91d
Takuya Yogo <62864518+yogotaku@users.noreply.github.com>
2022-01-24 16:45:29 +0900

    commit

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -38,31 +38,31 @@ class WebhookController < ApplicationController
     # end
     
     # head :ok
-    body = request.body.read
+    # body = request.body.read
 
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
+    # signature = request.env['HTTP_X_LINE_SIGNATURE']
+    # unless client.validate_signature(body, signature)
+    #   error 400 do 'Bad Request' end
+    # end
   
-    events = client.parse_events_from(body)
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
-        end
-      end
-    end
+    # events = client.parse_events_from(body)
+    # events.each do |event|
+    #   case event
+    #   when Line::Bot::Event::Message
+    #     case event.type
+    #     when Line::Bot::Event::MessageType::Text
+    #       message = {
+    #         type: 'text',
+    #         text: event.message['text']
+    #       }
+    #       client.reply_message(event['replyToken'], message)
+    #     when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+    #       response = client.get_message_content(event.message['id'])
+    #       tf = Tempfile.open("content")
+    #       tf.write(response.body)
+    #     end
+    #   end
+    # end
   
     # Don't forget to return a successful response
     "OK"
