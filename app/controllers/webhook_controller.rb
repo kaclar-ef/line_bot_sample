a61cf13d56cdbaf37e79bad3c25d6f1e6c1a9af0
Takuya Yogo <62864518+yogotaku@users.noreply.github.com>
2022-01-24 17:30:55 +0900

    commit

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,41 +2,41 @@ class WebhookController < ApplicationController
 
   CHANNEL = '#line_bot_test2'
   protect_from_forgery :except => [:callback]
-  
+
   def callback
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    # body = request.body.read
+    # signature = request.env['HTTP_X_LINE_SIGNATURE']
 
-    # リクエストがLINEのプラットフォームから送られたことを検証
-    unless line_client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
+    # # リクエストがLINEのプラットフォームから送られたことを検証
+    # unless line_client.validate_signature(body, signature)
+    #   error 400 do 'Bad Request' end
+    # end
 
-    evants = line_client.parse_events_from(body)
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MeesageType::Text
-          post_message(event.message['text'])
-        when Line::Bot::Event::MessageType::Image
-          image_response = line_client.get_message_content(event.message['id'])
-          file = File.open("/tmp/#{Time.current.strftime('%Y%m%d%H%M%S')}.jpg", 'w+b')
-          file.write(image_response.body)
+    # evants = line_client.parse_events_from(body)
+    # events.each do |event|
+    #   case event
+    #   when Line::Bot::Event::Message
+    #     case event.type
+    #     when Line::Bot::Event::MeesageType::Text
+    #       post_message(event.message['text'])
+    #     when Line::Bot::Event::MessageType::Image
+    #       image_response = line_client.get_message_content(event.message['id'])
+    #       file = File.open("/tmp/#{Time.current.strftime('%Y%m%d%H%M%S')}.jpg", 'w+b')
+    #       file.write(image_response.body)
 
-          slack_client.files_upload(channels: CHANNEL,
-                                    file: Faraday::UploadIO.new(file.path, 'image/jpeg'),
-                                    as_user: true,
-                                    title: File.basename(file.path),
-                                    filename: File.basename(file.path),
-                                    initial_comment: '写真が送信されました')
-        when Line::Bot::Event::MessageType::Video
-          post_message('ビデオが送信されました')
-        when Line::Bot::Event::MessageType::Sticker
-          post_message('スタンプが送信されました')
-        end
-      end
-    end
+    #       slack_client.files_upload(channels: CHANNEL,
+    #                                 file: Faraday::UploadIO.new(file.path, 'image/jpeg'),
+    #                                 as_user: true,
+    #                                 title: File.basename(file.path),
+    #                                 filename: File.basename(file.path),
+    #                                 initial_comment: '写真が送信されました')
+    #     when Line::Bot::Event::MessageType::Video
+    #       post_message('ビデオが送信されました')
+    #     when Line::Bot::Event::MessageType::Sticker
+    #       post_message('スタンプが送信されました')
+    #     end
+    #   end
+    # end
     
     head :ok
   end
